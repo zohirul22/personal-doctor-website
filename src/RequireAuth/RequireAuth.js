0e8dc6aa13088cfeb14d1e8c0de7304bdc196657
zohirulislam1 <codershahin84@gmail.com>
2022-10-07 17:25:06 +0600

    RequaAuth page update

--- src/RequireAuth/RequireAuth.js
+++ src/RequireAuth/RequireAuth.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import auth from '../firebase.init';
 import Loading from '../SocialPage/Loading/Loading'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const RequireAuth = ({ children }) => {
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
 
@@ -17,7 +20,22 @@ const RequireAuth = ({ children }) => {
 
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
-
+    if (!user.emailVerified) {
+        return <div className='text-center'>
+            <h2 className='text-danger'>Yours Email is not Verified</h2>
+            <h3 className='text-success'>Please Email is Verified</h3>
+            <button
+            className='btn btn-primary w-50'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+                }}
+            >
+                Verify email
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
+    }
 
     return children;
 };
